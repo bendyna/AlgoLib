040db2d059dfe3ac8b5b0ed1af61ff24f795db07
bendyna.ivan <bendyna.ivan@gmail.com>
2013-04-03 18:28:28 +0300

    added method squareDistance to avoid expensive operation "sqrt" where it's not necessary (compare distances for example)

--- src/geometry2D/DistancePoints.java
+++ src/geometry2D/DistancePoints.java
@@ -6,10 +6,15 @@ package geometry2D;
  * Date: 03.04.13
  */
 public class DistancePoints {
-    public static double distance(Point2D a, Point2D b){
+
+    public static double squareDistance(Point2D a, Point2D b){
         float dx = a.getX() - b.getX();
         float dy = a.getY() - b.getY();
-        return Math.sqrt(dx * dx + dy * dy);
+        return dx * dx + dy * dy;
+    }
+
+    public static double distance(Point2D a, Point2D b){
+        return Math.sqrt(squareDistance(a, b));
     }
     //TODO: check arithmetic overflow
     //TODO: unit tests
