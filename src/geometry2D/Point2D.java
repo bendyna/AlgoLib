da21456bf9f1e24b0d312dab42e7d4c785dd1ba2
bendyna.ivan <bendyna.ivan@gmail.com>
2013-04-03 15:01:26 +0300

    added class Point2D

---
+++ src/geometry2D/Point2D.java
@@ -0,0 +1,25 @@
+package geometry2D;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ivan.bendyna
+ * Date: 03.04.13
+ */
+public class Point2D {
+
+    final private float x;
+    final private float y;
+
+    public Point2D(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+}
